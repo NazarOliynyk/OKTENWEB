42459996467b44c33565e9939c0bb07652d40b3d
Your Name <nazar_lw@ukr.net>
2019-05-08 17:18:47 +0300

    minor changes

--- src/Lambdas/Main_1.java
+++ src/Lambdas/Main_1.java
@@ -1,24 +1,24 @@
-//package Lambdas;
-//
-//public class Main_1 {
-//
-//    public static void main(String[] args) {
-//
-//        Inter_1 inter_1_1 = new Inter_1() {
-//            @Override
-//            public void some() {
-//                System.out.println("Hello!");
-//            }
-//        };
-//
-//        Inter_1 inter_1_2=()-> System.out.println("Hello!");
-//        Inter_1 inter_1_3= ()->{
-//            System.out.println("Hello");
-//            System.out.println("!!!!");
-//        };
-//
-//        inter_1_1.some();
-//        inter_1_2.some();
-//        inter_1_3.some();
-//    }
-//}
+package Lambdas;
+
+public class Main_1 {
+
+    public static void main(String[] args) {
+
+        Inter_1 inter_1_1 = new Inter_1() {
+            @Override
+            public void some() {
+                System.out.println("Hello!");
+            }
+        };
+
+        Inter_1 inter_1_2=()-> System.out.println("Hello!");
+        Inter_1 inter_1_3= ()->{
+            System.out.println("Hello");
+            System.out.println("!!!!");
+        };
+
+        inter_1_1.some();
+        inter_1_2.some();
+        inter_1_3.some();
+    }
+}
--- src/PhoneBook_Map/Main.java
+++ src/PhoneBook_Map/Main.java
@@ -1,74 +1,74 @@
-package PhoneBook_Map;
-
-import java.util.*;
-
-public class Main {
-
-    public static void main(String[] args) {
-
-        TreeMap<User, TreeSet<PhoneNumber>> treeMap = new TreeMap<>();
-
-        TreeSet<PhoneNumber> phoneNumbers = new TreeSet<>();
-        System.out.println(phoneNumbers.isEmpty());
-
-        phoneNumbers.add(new PhoneNumber("home number", 8900988));
-        phoneNumbers.add(new PhoneNumber("work number", 324874386));
-        phoneNumbers.add(new PhoneNumber("fax", 87687342));
-        phoneNumbers.add(new PhoneNumber("cell phone", 21676));
-
-        treeMap.put(new User("Ivan", "Travkin"),phoneNumbers);
-
-        //System.out.println(treeMap);
-
-
-        Set<Map.Entry<User, TreeSet<PhoneNumber>>> entries = treeMap.entrySet();
-
-       //  System.out.println("entries "+entries);
-
-        Iterator<Map.Entry<User, TreeSet<PhoneNumber>>> iterator =entries.iterator();
-
-        while (iterator.hasNext()){
-            Map.Entry<User, TreeSet<PhoneNumber>> next = iterator.next();
-            Iterator<PhoneNumber> iterator1 = next.getValue().iterator();
-            while (iterator1.hasNext()){
-                PhoneNumber next1 = iterator1.next();
-                if(next1.getTypeOfNumber().equals("cell phone")){
-                    iterator1.remove();
-                }
-            }
-
-        }
-
-        for (Map.Entry<User, TreeSet<PhoneNumber>> userTreeSetEntry : treeMap.entrySet()) {
-            //userTreeSetEntry.getKey().setName();
-            //userTreeSetEntry.getKey().
-            for (Object o : userTreeSetEntry.getValue().toArray()) {
-                //System.out.println(o.toString());
-            }
-            if ( userTreeSetEntry.getValue().isEmpty()) {
-                //System.out.println(o.toString());
-            }
-            if (userTreeSetEntry.getKey().getName().equals("Ivan")){
-                userTreeSetEntry.getValue().add(new PhoneNumber("bankNumber", 456456));
-            }
-        }
-
-
-        //System.out.println(treeMap);
-
-        for (Map.Entry<User, TreeSet<PhoneNumber>> userTreeSetEntry : treeMap.entrySet()) {
-            final Iterator<PhoneNumber> iterator1 = userTreeSetEntry.getValue().iterator();
-            while (iterator1.hasNext()){
-                PhoneNumber number = iterator1.next();
-                System.out.println(number.getTypeOfNumber());
-            }
-        }
-
-       System.out.println(treeMap);
-
-    }
-
-
-
-
-}
+//package PhoneBook_Map;
+//
+//import java.util.*;
+//
+//public class Main {
+//
+//    public static void main(String[] args) {
+//
+//        TreeMap<User, TreeSet<PhoneNumber>> treeMap = new TreeMap<>();
+//
+//        TreeSet<PhoneNumber> phoneNumbers = new TreeSet<>();
+//        System.out.println(phoneNumbers.isEmpty());
+//
+//        phoneNumbers.add(new PhoneNumber("home number", 8900988));
+//        phoneNumbers.add(new PhoneNumber("work number", 324874386));
+//        phoneNumbers.add(new PhoneNumber("fax", 87687342));
+//        phoneNumbers.add(new PhoneNumber("cell phone", 21676));
+//
+//        treeMap.put(new User("Ivan", "Travkin"),phoneNumbers);
+//
+//        //System.out.println(treeMap);
+//
+//
+//        Set<Map.Entry<User, TreeSet<PhoneNumber>>> entries = treeMap.entrySet();
+//
+//       //  System.out.println("entries "+entries);
+//
+//        Iterator<Map.Entry<User, TreeSet<PhoneNumber>>> iterator =entries.iterator();
+//
+//        while (iterator.hasNext()){
+//            Map.Entry<User, TreeSet<PhoneNumber>> next = iterator.next();
+//            Iterator<PhoneNumber> iterator1 = next.getValue().iterator();
+//            while (iterator1.hasNext()){
+//                PhoneNumber next1 = iterator1.next();
+//                if(next1.getTypeOfNumber().equals("cell phone")){
+//                    iterator1.remove();
+//                }
+//            }
+//
+//        }
+//
+//        for (Map.Entry<User, TreeSet<PhoneNumber>> userTreeSetEntry : treeMap.entrySet()) {
+//            //userTreeSetEntry.getKey().setName();
+//            //userTreeSetEntry.getKey().
+//            for (Object o : userTreeSetEntry.getValue().toArray()) {
+//                //System.out.println(o.toString());
+//            }
+//            if ( userTreeSetEntry.getValue().isEmpty()) {
+//                //System.out.println(o.toString());
+//            }
+//            if (userTreeSetEntry.getKey().getName().equals("Ivan")){
+//                userTreeSetEntry.getValue().add(new PhoneNumber("bankNumber", 456456));
+//            }
+//        }
+//
+//
+//        //System.out.println(treeMap);
+//
+//        for (Map.Entry<User, TreeSet<PhoneNumber>> userTreeSetEntry : treeMap.entrySet()) {
+//            final Iterator<PhoneNumber> iterator1 = userTreeSetEntry.getValue().iterator();
+//            while (iterator1.hasNext()){
+//                PhoneNumber number = iterator1.next();
+//                System.out.println(number.getTypeOfNumber());
+//            }
+//        }
+//
+//       System.out.println(treeMap);
+//
+//    }
+//
+//
+//
+//
+//}
